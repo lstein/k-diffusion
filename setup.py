32a7a7775f7a2e3e33e8f3e1a4e735756375500f
Lincoln Stein <lincoln.stein@gmail.com>
2022-08-21 18:00:20 -0400

    added setup.py to allow for conda install

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='k-diffusion',
+    version='0.0.1',
+    description='k-diffusion sampler',
+    packages=find_packages(),
+    install_requires=[
+        'accelerate',
+        'clean-fid',
+        'einops',
+        'jsonmerge',
+        'kornia',
+        'Pillow',
+        'resize-right',
+        'scikit-image',
+        'scipy',
+        'torch',
+        'torchdiffeq',
+        'torchvision',
+        'tqdm',
+        'wandb',
+        'clip @ git+https://github.com/openai/CLIP.git@main#egg=clip'
+    ]
+)
