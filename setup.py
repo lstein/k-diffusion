a6a61776dd318ca4720569e01b32ecd0796dd465
Lincoln Stein <lincoln.stein@gmail.com>
2022-08-21 18:10:25 -0400

    try 4

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         'torchvision',
         'tqdm',
         'wandb',
-#        'CLIP @ git+https://github.com/openai/CLIP.git@main#egg=clip'
+        'clip @ git+https://github.com/openai/CLIP.git@main#egg=clip'
     ]
 )
